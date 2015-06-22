6e3d612fa5627ba427dacab323d5bcb1ad18aa24
Pierre Noizat <noizat@hotmail.com>
2015-06-22 20:36:28 +0200

    new landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,21 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( custom.css.scss )
+
+
+Rails.application.config.assets.precompile += %w( plugins/owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( plugins/jquery.peity.min.js )
+Rails.application.config.assets.precompile += %w( plugins/jquery.waypoints.min.js )
+Rails.application.config.assets.precompile += %w( plugins/smoothscroll.js )
+Rails.application.config.assets.precompile += %w( plugins/wow.min.js )
+Rails.application.config.assets.precompile += %w( plugins/contact.js )
+Rails.application.config.assets.precompile += %w( plugins/lightbox.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
+
+Rails.application.config.assets.precompile += %w( open-iconic/font/css/open-iconic-bootstrap.css )
+Rails.application.config.assets.precompile += %w( font-awesome/css/font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( plugins/lightbox.css )
+Rails.application.config.assets.precompile += %w( plugins/animate.css )
+Rails.application.config.assets.precompile += %w( plugins/owl.carousel.css )
+Rails.application.config.assets.precompile += %w( styles.css )
