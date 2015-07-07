d3355aa4cbcd3d2f9e4557416267f5ecc0a0baf4
davout <david.francois@webflows.fr>
2015-07-07 21:04:08 +0200

    Deploy ZzZ

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,13 +36,16 @@ set :keep_releases, 5
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  after :publishing, 'deploy:restart'
+
+  desc "Restart application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :mkdir, '-p', "#{ release_path }/tmp"
+      execute :touch, release_path.join("tmp/restart.txt")
     end
   end
 
+  after :finishing, 'deploy:cleanup'
 end
+
