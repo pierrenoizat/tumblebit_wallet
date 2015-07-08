3d8c356778874b555992f1a7a14613c28e2f8547
davout <david.francois@webflows.fr>
2015-07-08 15:55:11 +0200

    Deploy

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,1 @@
-require "rubygems"
-require "redis"
-
-#uri = URI.parse(ENV["REDISTOGO_URL"])
-#uri = URI.parse(URI.encode(ENV["REDISTOGO_URL"].strip))
-#$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-
-#$redis = Redis.new(:host => 'angelfish.redistogo.com', :port => 10315, :password => "652f4c27327c7cb52ac7042b0fb31ca2")
-$redis = Redis.new(:host => 'localhost', :port => 6379)
-
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => uri)
+$redis = Redis.new(YAML::load_file(File.join(Rails.root, 'config/redis.yml'))[Rails.env])
