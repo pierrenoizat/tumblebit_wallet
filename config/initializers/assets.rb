ea30366e63bada969f2c27e39e2cb2c110c9fe9c
Pierre Noizat <noizat@hotmail.com>
2016-10-23 20:55:30 +0200

    moved js script in form view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '3.0'
+Rails.application.config.assets.version = '3.1'
 
 Rails.application.config.assets.precompile += %w( plugins/owl.carousel.min.js )
 Rails.application.config.assets.precompile += %w( plugins/jquery.peity.min.js )
@@ -10,7 +10,7 @@ Rails.application.config.assets.precompile += %w( plugins/smoothscroll.js )
 Rails.application.config.assets.precompile += %w( plugins/wow.min.js )
 Rails.application.config.assets.precompile += %w( custom.js )
 
-Rails.application.config.assets.precompile += %w( scripts.js )
+# Rails.application.config.assets.precompile += %w( scripts.js )
 
 # Rails.application.config.assets.precompile += ['scripts.js']
 
