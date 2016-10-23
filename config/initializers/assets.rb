015c188b5d0493a4d1527d7ed4d686e2a4d2ccd5
Pierre Noizat <noizat@hotmail.com>
2016-10-23 20:51:22 +0200

    new config.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '2.0'
+Rails.application.config.assets.version = '3.0'
 
 Rails.application.config.assets.precompile += %w( plugins/owl.carousel.min.js )
 Rails.application.config.assets.precompile += %w( plugins/jquery.peity.min.js )
@@ -12,5 +12,5 @@ Rails.application.config.assets.precompile += %w( custom.js )
 
 Rails.application.config.assets.precompile += %w( scripts.js )
 
-Rails.application.config.assets.precompile += ['scripts.js']
+# Rails.application.config.assets.precompile += ['scripts.js']
 
